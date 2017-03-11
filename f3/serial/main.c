42efff3b40f3b44756be49d820d18dd96f323158
Parker Lusk <parkerclusk@gmail.com>
2017-03-11 16:41:40 -0700

    setup new example for serial; altered Makefile

---
+++ f3/serial/main.c
@@ -0,0 +1,31 @@
+#include <stm32f30x.h>
+#include <stm32f30x_rcc.h>
+#include <stm32f30x_gpio.h>
+ 
+#define LED    GPIO_Pin_8
+ 
+int main()
+{
+      long i;
+      GPIO_InitTypeDef gpio;
+ 
+      // Blue LED is connected to port E, pin 8 (AHB bus)
+      RCC_AHBPeriphClockCmd( RCC_AHBPeriph_GPIOB, ENABLE );
+ 
+      // Configure port E (LED)
+      GPIO_StructInit( &gpio );
+      gpio.GPIO_Mode = GPIO_Mode_OUT;
+      gpio.GPIO_Pin  = LED;
+      GPIO_Init( GPIOB, &gpio );
+ 
+      // Blinking LEDS
+      while(1)
+      {
+              // On
+              GPIO_SetBits( GPIOB, LED );
+              for( i = 0; i < 500000; i++ );
+              // All off
+              GPIO_ResetBits( GPIOB, LED );
+              for( i = 0; i < 500000; i++ );
+      }
+}
